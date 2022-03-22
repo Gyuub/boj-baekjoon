ac5c7d738329f6b028e77d0ecaff86199316755d
Gyub <hman0301@kakao.com>
2022-03-23 01:21:40 +0900

    알고리즘 스터디 데브매칭 2021  - 로또의 최고 순위와 최저 순위  - 행렬 테두리 회전하기

--- src/gyub/programmers/devmatching/Problem77484.java
+++ src/gyub/programmers/devmatching/Problem77484.java
@@ -3,13 +3,15 @@ package gyub.programmers.devmatching;
 
 /**
  * Description : 로또의 최고 순위와 최저 순위
+ *
  * @author Hman
  * @version 1.0.0
  * 작성일 2022-03-22
  */
 public class Problem77484 {
-    static boolean [] lottoNumber = new boolean[46];
-    static int [] winCountRank = new int[]{6, 6, 5, 4, 3, 2, 1};
+    static boolean[] lottoNumber = new boolean[46];
+    static int[] winCountRank = new int[]{6, 6, 5, 4, 3, 2, 1};
+
     public static void main(String[] args) {
 
     }
@@ -24,15 +26,15 @@ public class Problem77484 {
         for (int i = 0; i < lottos.length; i++) {
             int num = lottos[i];
 
-            if(num == 0 ) zero++;
+            if (num == 0) zero++;
 
-            if(lottoNumber[num]){
+            if (lottoNumber[num]) {
                 count++;
             }
         }
 
 
-        answer = new int[]{winCountRank[count+zero], winCountRank[count]};
+        answer = new int[]{winCountRank[count + zero], winCountRank[count]};
 
 
         return answer;
--- src/gyub/programmers/devmatching/Problem77485.java
+++ src/gyub/programmers/devmatching/Problem77485.java
@@ -4,25 +4,27 @@ import java.util.Arrays;
 
 /**
  * Description : 행렬 테두리 회전하기
+ *
  * @author Hman
  * @version 1.0.0
  * 작성일 2022-03-22
  */
 public class Problem77485 {
     static int[][] matrix;
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(6, 6, new int[][]{{2,2,5,4}, {3,3,6,6} , {5,1,6,3}})));
+        System.out.println(Arrays.toString(solution(6, 6, new int[][]{{2, 2, 5, 4}, {3, 3, 6, 6}, {5, 1, 6, 3}})));
     }
 
     public static int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int [queries.length];
-        matrix = new int[rows+1][columns+1];
+        int[] answer = new int[queries.length];
+        matrix = new int[rows + 1][columns + 1];
 
         //행렬
         int num = 1;
-        for (int i = 1; i < rows+1; i++) {
-            for (int j = 1; j < columns+1; j++) {
-                matrix[i][j] = num ++;
+        for (int i = 1; i < rows + 1; i++) {
+            for (int j = 1; j < columns + 1; j++) {
+                matrix[i][j] = num++;
             }
         }
 
@@ -38,7 +40,7 @@ public class Problem77485 {
         return answer;
     }
 
-    public static int rotation(int col1, int row1, int col2, int row2){
+    public static int rotation(int col1, int row1, int col2, int row2) {
         int temp = matrix[row1][col1];
         int min = 99999;
         for (int k = row1; k < row2; k++) {
